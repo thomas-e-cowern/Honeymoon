9c731677d43d171d7c6cbebef64330fcf57fbed0
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-27 15:26:05 -0500

    add Honeymoon project, update .gitignore

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Thomas Cowern on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
