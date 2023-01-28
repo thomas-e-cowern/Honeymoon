aab8a31bcf77882257ff6002b41dab40e0c1a58b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-28 14:19:52 -0500

    add FooterView

---
+++ Honeymoon/Honeymoon/FooterView.swift
@@ -0,0 +1,20 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Thomas Cowern on 1/28/23.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
