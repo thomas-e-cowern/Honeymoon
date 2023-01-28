673e09006840d8c935b9cd258ac997bd4c1b2a2a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-28 13:50:01 -0500

    add View folder and move ContentView

--- Honeymoon/Honeymoon/ContentView.swift
+++ Honeymoon/Honeymoon/ContentView.swift
@@ -2,20 +2,14 @@
 //  ContentView.swift
 //  Honeymoon
 //
-//  Created by Thomas Cowern on 1/27/23.
+//  Created by Thomas Cowern on 1/28/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
