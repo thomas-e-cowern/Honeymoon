8ec0cf3a081c894807d21fe9ffcadbe329f1407f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-28 14:22:46 -0500

    add Images to FooterView

--- Honeymoon/Honeymoon/FooterView.swift
+++ Honeymoon/Honeymoon/FooterView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct FooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
     }
 }
 
 struct FooterView_Previews: PreviewProvider {
     static var previews: some View {
         FooterView()
+            .previewLayout(.fixed(width: 375, height: 80))
     }
 }
