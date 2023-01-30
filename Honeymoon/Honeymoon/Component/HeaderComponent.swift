941e6d779778521280eb1f2bb90aed2731c9aa52
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-30 13:35:09 -0500

    update HeaderComponent with Capsule and Image

--- Honeymoon/Honeymoon/Component/HeaderComponent.swift
+++ Honeymoon/Honeymoon/Component/HeaderComponent.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct HeaderComponent: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 20) {
+            Capsule()
+                .frame(width: 120, height: 6)
+                .foregroundColor(.secondary)
+                .opacity(0.2)
+            
+            Image("logo-honeymoon")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 28)
+        }
     }
 }
 
 struct HeaderComponent_Previews: PreviewProvider {
     static var previews: some View {
         HeaderComponent()
+            .previewLayout(.fixed(width: 375, height: 128))
     }
 }
