f80e66a6a0e155ef5a32fd8382757e8e028e3f5e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-30 13:32:23 -0500

    add Component folder and HeaderComponent file

---
+++ Honeymoon/Honeymoon/Component/HeaderComponent.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderComponent.swift
+//  Honeymoon
+//
+//  Created by Thomas Cowern on 1/30/23.
+//
+
+import SwiftUI
+
+struct HeaderComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HeaderComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderComponent()
+    }
+}
