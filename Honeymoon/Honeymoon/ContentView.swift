bb09b42ae7fbd1bb40fd2d7f59228b7db63e53e3
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-31 11:27:16 -0500

    update ContentView with guide variables

--- Honeymoon/Honeymoon/ContentView.swift
+++ Honeymoon/Honeymoon/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - Properties
     @State var showAlert: Bool = false
+    @State var showGuide: Bool = false
     
     var body: some View {
         VStack {
-            HeaderView()
+            HeaderView(showGuideview: $showGuide)
             
             Spacer()
             
