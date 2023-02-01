308abb22a8cd650e635278cb3cfbddd1405e86f9
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-01 09:59:26 -0500

    add DragState to ContentView

--- Honeymoon/Honeymoon/Views/ContentView.swift
+++ Honeymoon/Honeymoon/Views/ContentView.swift
@@ -32,6 +32,40 @@ struct ContentView: View {
         return index == 0
     }
     
+    // MARK: - Drag states
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+            
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
             // MARK: - Header
