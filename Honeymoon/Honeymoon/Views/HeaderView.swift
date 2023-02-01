c0492829590a9d976148308f0a2408b9909a9630
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-01 08:17:31 -0500

    update HeaderView Button and sheet

--- Honeymoon/Honeymoon/Views/HeaderView.swift
+++ Honeymoon/Honeymoon/Views/HeaderView.swift
@@ -10,17 +10,22 @@ import SwiftUI
 struct HeaderView: View {
     // MARK: - Properties
     @Binding var showGuideview: Bool
+    @Binding var showInfoView: Bool
     
     var body: some View {
         HStack {
             Button {
                 // Action
                 print("Information")
+                self.showInfoView.toggle()
             } label: {
                 Image(systemName: "info.circle")
                     .font(.system(size: 24, weight: .regular))
             }
             .tint(.primary)
+            .sheet(isPresented: $showInfoView) {
+                InfoView()
+            }
             
             Spacer()
             
@@ -51,9 +56,10 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     @State static var showGuide: Bool = false
+    @State static var showInfoView: Bool = false
     
     static var previews: some View {
-        HeaderView(showGuideview: $showGuide)
+        HeaderView(showGuideview: $showGuide, showInfoView: $showInfoView)
             .previewLayout(.fixed(width: 375, height: 80))
     }
 }
