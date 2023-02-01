091eef30faf1bac03ce048f501f3693c59ad30f2
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-01 08:12:30 -0500

    complete InfoView

--- Honeymoon/Honeymoon/Views/InfoView.swift
+++ Honeymoon/Honeymoon/Views/InfoView.swift
@@ -25,11 +25,7 @@ struct InfoView: View {
                     .fontWeight(.black)
                     .modifier(TitleModifier())
                 
-                HStack {
-                    Text("Photos").foregroundColor(.gray)
-                    Spacer()
-                    Text("Unsplash")
-                }
+                CreditsView()
                 
                 Spacer(minLength: 10)
                 
@@ -84,6 +80,27 @@ struct RowAppInfoView: View {
                 Text(itemTwo)
             }
             Divider()
+        }
+    }
+}
+
+struct CreditsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Photos").foregroundColor(.gray)
+                Spacer()
+                Text("Unsplash")
+            }
+            
+            Divider()
+            
+            Text("Photographers")
+                .foregroundColor(.gray)
+            
+            Text("Shifaaz Shamoon (Maldives), Grillot Edouard (France), Evan Wise (Greece), Christoph Schulz (United Arab Emirates), Andrew Coelho (USA), Damiano Baschiera (Italy), Daniel Olah (Hungary), Andrzej Rusinowski (Poland), Lucas Miguel (Slovenia), Florencia Potter (Spain), Ian Simmonds (USA), Ian Keefe (Canada), Denys Nevozhai (Thailand), David Köhler (Italy), Andre Benz (USA), Alexandre Chambon (South Korea), Roberto Nickson (Mexico), Ajit Paul Abraham (UK), Jeremy Bishop (USA), Davi Costa (Brazil), Liam Pozz (Australia)")
+              .multilineTextAlignment(.leading)
+              .font(.footnote)
         }
     }
 }
