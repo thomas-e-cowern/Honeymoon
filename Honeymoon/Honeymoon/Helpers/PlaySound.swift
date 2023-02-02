8fd3292ffb8cd2ab8949791b33f5ca8101bf1d52
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-02 08:57:20 -0500

    add Helpers folder and PlaySound swift fiel

---
+++ Honeymoon/Honeymoon/Helpers/PlaySound.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySound.swift
+//  Honeymoon
+//
+//  Created by Thomas Cowern on 2/2/23.
+//
+
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("ERROR: Could not find and play the sound file")
+        }
+    }
+}
+
