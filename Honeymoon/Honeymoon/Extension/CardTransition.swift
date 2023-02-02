c5d58cf17eaae650f7de8bb9e023904cb1286ee5
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-02 08:42:00 -0500

    add Extension folder and CardTransition extension

---
+++ Honeymoon/Honeymoon/Extension/CardTransition.swift
@@ -0,0 +1,18 @@
+//
+//  CardTransition.swift
+//  Honeymoon
+//
+//  Created by Thomas Cowern on 2/2/23.
+//
+
+import SwiftUI
+
+extension AnyTransition {
+    static var trailingBottom: AnyTransition {
+        AnyTransition.asymmetric(insertion: .identity, removal: AnyTransition.move(edge: .trailing).combined(with: .move(edge: .bottom)))
+    }
+    
+    static var leadingBottom: AnyTransition {
+        AnyTransition.asymmetric(insertion: .identity, removal: AnyTransition.move(edge: .leading).combined(with: .move(edge: .bottom)))
+    }
+}
